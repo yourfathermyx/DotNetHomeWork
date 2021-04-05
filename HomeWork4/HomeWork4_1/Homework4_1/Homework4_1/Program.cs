b6df3b6903d09f0e2f1dbe043eeeeea2a40597c9
宇鑫 穆 <1363902011@qq.com>
2021-04-05 19:19:18 +0800

    HomeWork4

---
+++ HomeWork4/HomeWork4_1/Homework4_1/Homework4_1/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //整型list
+            GenericList<int> genericList = new GenericList<int>();
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                genericList.Add(random.Next(0, 10));
+            }
+            //show
+            genericList.ForEach(Console.WriteLine);
+            //求和
+            int sum = 0;
+            genericList.ForEach(x => sum += x);
+
+            //求max
+            int max = genericList.Head.Data;
+            genericList.ForEach(
+                x =>
+                {
+                    if (max < x)
+                        max = x;
+                }
+            );
+
+            //求min
+            int min = genericList.Head.Data;
+            genericList.ForEach(
+                x =>
+                {
+                    if (x < min) min = x;
+                }
+            );
+
+            Console.WriteLine($"sum:{sum}");
+            Console.WriteLine($"max:{max}");
+            Console.WriteLine($"min:{min}");
+
+        }
+    }
+
+    public class GenericList<T> where T : IComparable
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head { get => head; }
+
+        public void Add(T t)
+        {
+            Node<T> node = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = node;
+            }
+            else
+            {
+                tail.Next = node;
+                tail = node;
+            }
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            Node<T> node = head;
+            while (node != null)
+            {
+                action(node.Data);
+                node = node.Next;
+            }
+
+        }
+
+    }
+
+    public class GenericList<T> where T : IComparable
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head { get => head; }
+
+        public void Add(T t)
+        {
+            Node<T> node = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = node;
+            }
+            else
+            {
+                tail.Next = node;
+                tail = node;
+            }
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            Node<T> node = head;
+            while (node != null)
+            {
+                action(node.Data);
+                node = node.Next;
+            }
+
+        }
+
+    }
+}
---
+++ HomeWork4/HomeWork4_2/HomeWork4_2/HomeWork4_2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4_2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock();
+            Console.WriteLine("闹钟启动，滴答滴答...");
+            Console.Write("请输入设置的闹钟时间(hh-mm-ss):");
+            string alarmTime = Console.ReadLine();
+            DateTime alarmDateTime = DateTime.ParseExact(alarmTime, "HH-m-s", System.Globalization.CultureInfo.CurrentCulture);
+            clock.AlarmRun(alarmDateTime);
+            //clock.Run();
+
+
+        }
+    }
+
+    public delegate void TickHanlder();
+    public delegate void AlarmHanlder(DateTime dataTime);
+    public class Clock
+    {
+        public event TickHanlder Tick;
+        public event AlarmHanlder Alarm;
+        public void TimeFly()
+        {
+            Console.Write(". ");
+            Thread.Sleep(1000);
+        }
+
+        public void Run()
+        {
+            Tick();
+        }
+
+        public void AlarmRemind(DateTime dataTime)
+        {
+            while (Math.Abs(dataTime.Ticks - DateTime.Now.Ticks) > 10e7)//1tick=10e-7s
+            {
+                Run();
+            }
+            Console.WriteLine();
+            Console.WriteLine("叮铃铃叮铃铃！！！闹钟响啦！！！");
+            for (int i = 0; i < 10; i++)//响铃后，再运行十秒钟
+            {
+                Run();
+            }
+            Console.WriteLine();
+        }
+
+        public void AlarmRun(DateTime dataTime)
+        {
+            Alarm(dataTime);
+        }
+        public Clock()
+        {
+            Tick += TimeFly;
+            Alarm += AlarmRemind;
+        }
+    }
+}
